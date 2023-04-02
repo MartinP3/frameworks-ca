fc6cf0034873120114cc1cc084a3b32a6aefb935
Martin P <70173574+MartinP3@users.noreply.github.com>
2023-04-02 21:21:21 +0200

    made Checkout page temporarily defunct

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,5 +1,11 @@
-import { CheckoutPage } from "../components/Checkout";
+// import { CheckoutPage } from "../components/Checkout";
 
 export function Checkout() {
-  return <CheckoutPage />;
+  return (
+    // <CheckoutPage />
+    <div>
+      <h1>Would be Checkout page</h1>
+      <h2>I'm sorry I couldn't figure this out.</h2>
+    </div>
+  );
 }
