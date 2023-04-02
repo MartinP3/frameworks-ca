a911e87267e1df3a9a0612b81895de3c7efff548
Martin P <70173574+MartinP3@users.noreply.github.com>
2023-04-02 21:42:17 +0200

    updated the 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export function NotFound() {
-  return <h1>Sowweee this isn't a page :c</h1>;
+  return (
+    <>
+      <h1>Sorry this isn't a page</h1>
+      <button>
+        <Link to="/">Return home!</Link>
+      </button>
+    </>
+  );
 }
