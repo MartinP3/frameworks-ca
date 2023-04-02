53ba3b3bf55d1f3a6d14063e1c52d198f5167936
Martin P <70173574+MartinP3@users.noreply.github.com>
2023-04-02 20:35:33 +0200

    giving up on checkout :(

--- src/components/Checkout/index.jsx
+++ src/components/Checkout/index.jsx
@@ -1,34 +1,33 @@
-// import { useCart } from '../../hooks/useCart';
-import { useEffect, useState } from 'react';
-// import * as S from "./index.styled"
+// // import { useCart } from '../../hooks/useCart';
+// import { useEffect, useState } from 'react';
+// // import * as S from "./index.styled"
 
-export function CheckoutPage()
-{
-  const cartData = localStorage.getItem("cart")
-  console.log(cartData)
-  const parsedData = JSON.parse(cartData) || {};
-  console.log(parsedData)
-  const [data, setData] = useState([]);
+// export function CheckoutPage()
+// {
+//   const cartData = localStorage.getItem("cart")
+//   const parsedData = JSON.parse(cartData) || {};
+//   console.log(parsedData)
+//   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    async function fetchProducts() {
-      const response = await fetch(parsedData);
-      const json = await response.json();
-      setData([json]);
-    }
-    fetchProducts();
-  }, [])
+//   useEffect(() => {
+//     async function fetchProducts() {
+//       const response = await fetch(parsedData);
+//       const json = await response.json();
+//       setData([json]);
+//     }
+//     fetchProducts();
+//   }, [])
 
-  return (
-    <main className='checkoutPage'>
-      <h1>Checkout Page</h1>
-      <div className='checkoutInfo'>
-        {data.map(product => (
-          <div key={product.id}>
-            <h2>{product.title}</h2>
-          </div>
-        ))}
-      </div>
-    </main>
-  );
-}
+//   return (
+//     <main className='checkoutPage'>
+//       <h1>Checkout Page</h1>
+//       <div className='checkoutInfo'>
+//         {data.map(product => (
+//           <div key={product.id}>
+//             <h2>{product.title}</h2>
+//           </div>
+//         ))}
+//       </div>
+//     </main>
+//   );
+// }
