282366ff8aa913929099bc242546b92297df493f
Martin P <70173574+MartinP3@users.noreply.github.com>
2023-04-02 21:42:48 +0200

    edited content for checkout success page

--- src/pages/CheckoutSuccess.js
+++ src/pages/CheckoutSuccess.js
@@ -1,8 +1,9 @@
 export function CheckoutSuccess() {
   return (
     <div>
-      <h1>Checkout page</h1>
+      <h1>Checkout success page</h1>
       <p>Check it out!</p>
+      <p>editors note: :c</p>
     </div>
   );
 }
