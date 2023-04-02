b56f7055a2c4bb71fc04f71cd78dfabae6759112
Martin P <70173574+MartinP3@users.noreply.github.com>
2023-04-02 19:57:33 +0200

    file for checkout page content

---
+++ src/components/Checkout/index.jsx
@@ -0,0 +1,34 @@
+// import { useCart } from '../../hooks/useCart';
+import { useEffect, useState } from 'react';
+// import * as S from "./index.styled"
+
+export function CheckoutPage()
+{
+  const cartData = localStorage.getItem("cart")
+  console.log(cartData)
+  const parsedData = JSON.parse(cartData) || {};
+  // console.log(parsedData)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await fetch(parsedData);
+      const json = await response.json();
+      setData([json]);
+    }
+    fetchProducts();
+  }, [])
+
+  return (
+    <main className='checkoutPage'>
+      <h1>Checkout Page</h1>
+      <div className='checkoutInfo'>
+        {data.map(product => (
+          <div key={product.id}>
+            <h2>{product.title}</h2>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
